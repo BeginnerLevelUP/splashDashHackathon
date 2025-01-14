2a0f66f573a2953ecd78cb6041202045946bc6cc
Damion Morgan <morganinc5680@gmail.com>
2025-01-14 17:34:27 -0500

    added a example of enable

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -16,6 +16,24 @@ export {useUsers}
 
 
 //use enable property to fetch data after a certain condition is met
+// function Todos() {
+//     const [filter, setFilter] = React.useState('')
 
+//     const { data } = useQuery({
+//         queryKey: ['todos', filter],
+//         queryFn: () => fetchTodos(filter),
+//         // ⬇️ disabled as long as the filter is empty
+//         enabled: !!filter
+//     })
+
+//     return (
+//         <div>
+//         // 🚀 applying the filter will enable and execute the query
+//         <FiltersForm onApply= { setFilter } />
+//         { data && <TodosTable data={ data }
+// } />
+//     </div>
+//   )
+// }
 //for creating data use useMutation add in onSucess and onError queryClient.setQueryData([queryKey,id], data) to update the cache
 //pagination keeprpeviousdata:true property to keep the previous data
